ec7973248859d37fc2391ee8564fa01588255e49
Costas Basdekis <costas.basdekis@gmail.com>
2014-05-18 13:20:03 +0100

    Paper.js: Use compound path for drawing walls

--- js/demo/create-walls.js
+++ js/demo/create-walls.js
@@ -8,7 +8,7 @@ var ShadowsApp = (function defineShadowsApp(obj, _) {
 	App.createWalls = function createWalls() {
 		this.addOuterBox();
 
-		this.paper.wallsPath = new _.Path();
+		this.paper.wallsPath = new _.CompoundPath();
 		this.linesToPath(this.walls.lines, this.paper.wallsPath);
 	};
 
@@ -16,16 +16,29 @@ var ShadowsApp = (function defineShadowsApp(obj, _) {
 		this.walls.addBox([{x: 10, y: 10}, {x: 590, y: 590}]);
 	};
 
-	App.linesToPath = function linesToPath(lines, path) {
-		path.strokeColor = 'black';
+	App.linesToPath = function linesToPath(lines, compoundPath) {
+		compoundPath.strokeColor = 'black';
 
+		var path, prevLine, newPath;
 		for (var i = 0 ; i < lines.length ; i++) {
 			var line = lines[i];
 
 			var start = new _.Point(line.start.x, line.start.y);
 			var end = new _.Point(line.end.x, line.end.y);
 
-			path.moveTo(start);
+			if (!prevLine) {
+				newPath = true;
+			} else {
+				if (!prevLine.end.equals([line.start])) {
+					newPath = true;
+				}
+			}
+
+			if (newPath) {
+				path = new _.Path();
+				compoundPath.children.push(path);
+				path.moveTo(start);
+			}
 			path.lineTo(end);
 		}
 	};
